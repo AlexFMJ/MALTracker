6aafb3ff70bb35784bbab9cd107653931b8aa5c4
Protogazer <alexfmjohnson@gmail.com>
2023-11-21 19:50:33 -0700

    notes for later

---
+++ test_extension/content_scripts/requestListener.js
@@ -0,0 +1,10 @@
+/**  
+ * TODO: upon reaching the api authentication URL,
+ * Launch a script that will read post and get requests being made
+ * OR
+ * read URL to send
+*/
+
+/**
+ * Parse request/URL for code info, save to storage
+ */
