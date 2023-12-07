81befa150de4417707b2d49431680c8752abb258
Protogazer <alexfmjohnson@gmail.com>
2023-12-06 23:10:25 -0700

    notes

--- test_extension/content_scripts/crunchyroll_parse.js
+++ test_extension/content_scripts/crunchyroll_parse.js
@@ -64,3 +64,19 @@ observer.observe(target, config)
 // listen for GET request from:
 // #://crunchyroll.com/content/v2/cms*
 // should be json blob with episode name and number
+
+function getAnimeInfo(response) {
+
+}
+
+const target = "*://crunchyroll.com/content/v2/cms/objects/*";
+
+browser.webRequest.onCompleted.addListener(
+    getAnimeInfo(),
+    {urls: target}, // only target the MAL auth URL
+    ["responseHeaders"]
+    // ["blocking"] // Maybe block the request here?
+);
+
+// Im trying to read the response body, which can only be read from once ( https://www.reddit.com/r/sveltejs/comments/15up9co/how_can_i_intercept_and_modify_the_body_of_a/ )
+// I'll need to create a clone of it and submit that to the site, along with original headers (?) may not be needed if just reading
